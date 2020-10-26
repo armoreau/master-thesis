a7a77ced001defb061bd3b7298325cac1c71fe72
armoreau <arthur.moreau@student.uclouvain.be>
2020-10-26 17:12:10 +0300

    Add files via upload

--- MyODE45.py
+++ MyODE45.py
@@ -1,5 +1,13 @@
 import numpy as np
 
+# Multiply steps computed from asymptotic behaviour of errors by this.
+SAFETY = 0.9
+
+MIN_FACTOR = 0.2  # Minimum allowed decrease in a step size.
+MAX_FACTOR = 10  # Maximum allowed increase in a step size.
+
+ERROR_EXPONENT = -1 / (4 + 1) # 4 est l'ordre de l'erreur d'estimation
+
 def myODE45(fun ,t_span, y0, t_eval) :
 
     #Explicit Runge-kutta method order 5
@@ -14,97 +22,134 @@ def myODE45(fun ,t_span, y0, t_eval) :
     [9017/3168, -355/33, 46732/5247, 49/176, -5103/18656, 0],
     [35/384, 0, 500/1113, 125/192, -2187/6784, 11/84]])
 
-    B_1 = np.array([35/384, 0, 500/1113, 125/192, -2187/6784, 11/84, 0])#next point
-    B_2 = np.array([5179/57600, 0, 7571/16695, 393/640, -92097/339200, 187/2100, 1/40])#estimation
+    B = np.array([35/384, 0, 500/1113, 125/192, -2187/6784, 11/84, 0])#next point
+    E = np.array([5179/57600, 0, 7571/16695, 393/640, -92097/339200, 187/2100, 1/40])#estimation
     C = np.array([0, 1/5, 3/10, 4/5, 8/9, 1, 1])
     
     tol = 0.0001
-    order = 5
+    order = 4
     
     size_y0 = np.size(y0)
     
     output_y = np.zeros((size_y0,1))
     output_t = np.zeros((1,1))
-    
-    for j in range(size_y0):
         
-        output_y[j,0] = y0[j]
-        output_t[0] = t_span[0]
+    output_y[:,0] = y0
+    output_t[0] = t_span[0]
         
-        t = t_span[0]
-        estimation_error = 0
+    t = t_span[0]
+    y=y0
         
-        i=0
-        while t < t_span[1] :
+    h=0
+    while t < t_span[1] :
             #step size
-            if t==t_span[0] :
-                h = (t_span[1]-t_span[0])/1000 #To discuss
-            else :
-                h = h*np.abs(tol/estimation_error)**(1/(order+1))
-            if t+h > t_span[1] : #on ne depasse pas la borne d integration
-                h = t_span[1] - t
+        if t==t_span[0] :
+            direction = 1
+            f0 = np.array(fun(t,y0))
+            h = choose_first_step(fun, t, y0, f0, direction, order)
             
-            next_t = np.array([[t+h]])
-            output_t = np.concatenate((output_t,next_t),axis=1)
-            
-            #7 stages
-            k0 = fun(t,output_y[j,i])
-            k1 = fun(t+C[1]*h,output_y[j,i]+h*(A[1,0]*k0))
-            k2 = fun(t+C[2]*h,output_y[j,i]+h*(A[2,0]*k0+A[2,1]*k1))
-            k3 = fun(t+C[3]*h,output_y[j,i]+h*(A[3,0]*k0+A[3,1]*k1+A[3,2]*k2))
-            k4 = fun(t+C[4]*h,output_y[j,i]+h*(A[4,0]*k0+A[4,1]*k1+A[4,2]*k2+A[4,3]*k3))
-            k5 = fun(t+C[5]*h,output_y[j,i]+h*(A[5,0]*k0+A[5,1]*k1+A[5,2]*k2+A[5,3]*k3+ A[5,4]*k4))
-            k6 = fun(t+C[6]*h,output_y[j,i]+h*(A[6,0]*k0+A[6,1]*k1+A[6,2]*k2+A[6,3]*k3+ A[6,4]*k4+ A[6,5]*k5))
+        max_step = t_span[1] - t_span[0]
+        rtol = 1e-3#100 * EPS
+        atol = 1e-6#4 * EPS
 
-            next_y = np.array([[output_y[j,i] + h*(k0*B_1[0] + k1*B_1[1] + k2*B_1[2] + k3*B_1[3] + k4*B_1[4] + k5*B_1[5] +k6*B_1[6])]])
-            output_y = np.concatenate((output_y,next_y),axis=1) #si y0 a plusieurs dim ca risque de foirer
-            estimation = output_y[j,i] + h*(k0*B_2[0] + k1*B_2[1] + k2*B_2[2] + k3*B_2[3] + k4*B_2[4] + k5*B_2[5] +k6*B_2[6])
+        min_step = 10 * np.abs(np.nextafter(t, direction * np.inf) - t)
             
-            estimation_error = output_y[j,i+1] - estimation
-            
-            i=i+1
-            t=t+h
-            
-        if t_eval is None :
-            return (output_t, output_y);
-        else:
-            size_t_eval = np.size(t_eval)
-            new_output_y = np.zeros((size_y0,size_t_eval))
-            
-            k=0
-            
-            for j in range(size_y0):
-                for i in range(size_t_eval):
-                    
-                    """
-                    while not (output_t[j,k] <= t_eval[i] and t_eval[i] <= output_t[j,k+1]) :
-                        k=k+1
-                        
-                    dist1 = t_eval[i]-output_t[j,k]
-                    dist2 = output_t[j,k+1]-t_eval[i]
-                    dist = dist1+dist2
-                    
-                    new_output_y[j,i] = (dist2/dist)*(output_y[j,k]+ fun(output_t[j,k],output_y[j,k])*(t_eval[i]-output_t[j,k])) + (dist1/dist)* (output_y[j,k+1] - fun(output_t[j,k+1],output_y[j,k+1])*(output_t[j,k]-t_eval[i]))"""
-                    """
-                    while not (t_eval[i] <= output_t[j,k+1]) :
-                        k=k+1
-                    
-                    new_output_y[j,i] = output_y[j,k] + fun(output_t[j,k],output_y[j,k])*(t_eval[i]-output_t[j,k])"""
-                                    
-                    while not (output_t[j,k] <= t_eval[i] and t_eval[i] <= output_t[j,k+1]) :
-                        k=k+1
-                        
-                    #calcul de la valeur de new_output_y[j,i] (A modifier approximation inexacte)
-                    dist1 = t_eval[i]-output_t[j,k]
-                    dist2 = output_t[j,k+1]-t_eval[i]
-                    dist = dist1+dist2
-                    
-                    new_output_y[j,i] = (dist2/dist)*output_y[j,k]+ (dist1/dist)*output_y[j,k+1]
+        if h > max_step:
+            h = max_step
+        elif h < min_step:
+            h = min_step
 
-                    
-            return (t_eval, new_output_y);
+        step_accepted = False
+        step_rejected = False
+            
+        while not step_accepted:
+
+            h = h*direction
+            next_t = t + h
+
+            if direction * (next_t - t_span[1]) > 0:
+                next_t = t_span[1]
+
+            h = next_t - t
+            h = np.abs(h)
+
+            next_y, estimation_error = step(fun,t,y,h,A,B,C,E)
+            scale = atol + np.maximum(np.abs(y), np.abs(next_y)) * rtol
+                           
+            error_norm = norm(estimation_error / scale)
+
+            if error_norm < 1:
+                if error_norm == 0:
+                    factor = MAX_FACTOR
+                else:
+                    factor = min(MAX_FACTOR, SAFETY * error_norm ** ERROR_EXPONENT)
+
+                if step_rejected:
+                    factor = min(1, factor)
+
+                h *= factor
+
+                step_accepted = True
+            else:
+                h *= max(MIN_FACTOR, SAFETY * error_norm ** ERROR_EXPONENT)
+                step_rejected = True
+            
+        #next_y = step(fun,t,y,h,A,B,C)
+        #max_estimation_error = step_error(fun,t,y,h,A,B,C,E)
+        
+        to_concatanate_y = np.transpose(np.array([next_y]))
+        output_y = np.concatenate((output_y,to_concatanate_y),axis=1)
+        
+        #next_t = t+h
+        to_concatanate_t = np.array([[next_t]])
+        output_t = np.concatenate((output_t,to_concatanate_t),axis=1)
+            
+        y = next_y
+        t = next_t
+            
+    return (output_t[0], output_y);  
+        
+def choose_first_step(fun, t0, y0, f0, direction, order):
+    EPS = np.finfo(float).eps
+    rtol = 1e-3#100 * EPS
+    atol = 1e-6#4 * EPS
+
+    scale = atol + np.abs(y0) * rtol
+    d0 = norm(y0 / scale)
+    d1 = norm(f0 / scale)
+    if d0 < 1e-5 or d1 < 1e-5:
+        h0 = 1e-6
+    else:
+        h0 = 0.01 * d0 / d1
+
+    y1 = y0 + h0 * direction * f0
+    f1 = fun(t0 + h0 * direction, y1)
+    d2 = norm((f1 - f0) / scale) / h0
+
+    if d1 <= 1e-15 and d2 <= 1e-15:
+        h1 = max(1e-6, h0 * 1e-3)
+    else:
+        h1 = (0.01 / max(d1, d2)) ** (1 / (order + 1))
+
+    return min(100 * h0, h1)
     
-    return [output_t, output_y];
+def norm(x):
+    """Compute RMS norm."""
+    return np.linalg.norm(x) / x.size ** 0.5
 
-        
-        
+def step(fun,t,y,h,A,B,C,E): #a ameliore (pas besoin de calculer 2 fois les coeff K)
+    
+    k0 = np.array(fun(t,y))
+    k1 = np.array(fun(t+C[1]*h,y+h*(A[1,0]*k0)))
+    k2 = np.array(fun(t+C[2]*h,y+h*(A[2,0]*k0+A[2,1]*k1)))
+    k3 = np.array(fun(t+C[3]*h,y+h*(A[3,0]*k0+A[3,1]*k1+A[3,2]*k2)))
+    k4 = np.array(fun(t+C[4]*h,y+h*(A[4,0]*k0+A[4,1]*k1+A[4,2]*k2+A[4,3]*k3)))
+    k5 = np.array(fun(t+C[5]*h,y+h*(A[5,0]*k0+A[5,1]*k1+A[5,2]*k2+A[5,3]*k3+ A[5,4]*k4)))
+    k6 = np.array(fun(t+C[6]*h,y+h*(A[6,0]*k0+A[6,1]*k1+A[6,2]*k2+A[6,3]*k3+ A[6,4]*k4+ A[6,5]*k5)))
+    
+    next_y = y + h*(k0*B[0] + k1*B[1] + k2*B[2] + k3*B[3] + k4*B[4] + k5*B[5] +k6*B[6])
+    estimation = y + h*(k0*E[0] + k1*E[1] + k2*E[2] + k3*E[3] + k4*E[4] + k5*E[5] +k6*E[6])
+    estimation_error = next_y - estimation
+
+    
+    return next_y, estimation_error
--- run_ODE45.py
+++ run_ODE45.py
@@ -5,172 +5,56 @@ import matplotlib.pyplot as plt
 import time
 
 
-def diff_expo(t, y): return -0.5 * y
-def expo(t) : return 2*np.exp((-1/2)*t)
+def eq1(t, y):
+    return [y[1]+y[2], -y[0]+2*y[1]+y[2], y[0]+y[2]]
+def sol1(t) : #CI : y[0](0) = 2, y[1](0) = 4, y[2](0) = 6,
+    return [-3+5*np.exp(2*t), -3+2*np.exp(t)+5*np.exp(2*t), 3-2*np.exp(t)+5*np.exp(2*t)]
 
-def diff_sin(t,y) : return np.cos(t)
-def sin(t) : return np.sin(t)
+def eq2(t, y):
+    return -0.5 * y[0]
+def sol2(t) : #condition init y[0] = 2
+    return 2*np.exp((-1/2)*t) 
 
-Nbr_pts = 101
-t = np.linspace(0, 10, Nbr_pts)
+######################################### solve_ivp 
 
-######################################### Evaluation exacte
-true_sol_expo = np.zeros(Nbr_pts)
-true_sol_sin = np.zeros(Nbr_pts)
-for i in range(Nbr_pts) :
-    true_sol_expo[i] = expo(t[i])
-    true_sol_sin[i] = sin(t[i])
+tps1 = time.perf_counter()
+solu = solve_ivp(eq2, [0, 100], [2])
+tps2 = time.perf_counter()
+total_time = tps2 - tps1
+
+######################################### my_ODE45
+
+my_tps1 = time.perf_counter()
+[t, y] = myODE45(eq2, [0, 100], [2], None)
+my_tps2 = time.perf_counter()
+my_total_time = my_tps2 - my_tps1
+
+######################################### real evaluation
+
+real_sol = np.zeros((np.size(t)))
+
+for i in range(np.size(t)) :
+    real_sol[i] = sol2(t[i])
     
-    
-######################################### Approximation par solve_ivp
-print("===================")
+######################################### error
 
-tps1_expo = time.perf_counter()
-sol_expo = solve_ivp(diff_expo, [0, 10], [2], t_eval=t)
-tps2_expo = time.perf_counter()
+error = np.abs(real_sol - solu.y)
+my_error = np.abs(real_sol - y)
 
-tps1_sin = time.perf_counter()
-sol_sin = solve_ivp(diff_sin, [0, 10], [0], t_eval=t)
-tps2_sin = time.perf_counter()
+#########################################
 
-total_time_expo = tps2_expo - tps1_expo
-total_time_sin = tps2_sin - tps1_sin 
+print('total_time_solve_ivp = ')
+print(total_time)
+print('my_total_time_myODE45 = ')
+print(my_total_time)
 
-erreur_expo = np.abs(true_sol_expo - sol_expo.y[0])
-erreur_sin = np.abs(true_sol_sin - sol_sin.y[0])
-    
-######################################### Mon approximation
-print("===================")
-
-my_tps1_expo = time.perf_counter()
-(my_sol_expo_t,my_sol_expo_y) = myODE45(diff_expo, [0, 10], [2], t)
-my_tps2_expo = time.perf_counter()
-
-my_tps1_sin = time.perf_counter()
-(my_sol_sin_t,my_sol_sin_y) = myODE45(diff_sin, [0, 10], [0], t)
-my_tps2_sin = time.perf_counter()
-
-my_total_time_expo = my_tps2_expo - my_tps1_expo
-my_total_time_sin = my_tps2_sin - my_tps1_sin 
-
-my_erreur_expo = np.abs(true_sol_expo - my_sol_expo_y[0])
-my_erreur_sin = np.abs(true_sol_sin - my_sol_sin_y[0])
-
-
-######################################### Plot
-
+#########################################
 fig = plt.figure()
 plt.title('EDO : dy/dt = -0.5 * y         y(0) = 2         sol : y(t) = 2*exp(-t/2)')
 plt.xlabel('t')
 plt.ylabel('approximation_error')
 plt.yscale('log')
-plt.plot(t,erreur_expo,label='solve_ivp_error')
-plt.plot(t,my_erreur_expo,label='myODE45_error')
+plt.plot(t,error[0],label='solve_ivp_error')
+plt.plot(t,my_error[0],label='myODE45_error')
 plt.legend()
-
-fig2 = plt.figure()
-plt.title('EDO : dy/dt = cos(t)       y(0) = 0        sol : y(t) = sin(t)')
-plt.xlabel('t')
-plt.ylabel('approximation_error')
-plt.yscale('log')
-plt.plot(t,erreur_sin,label='solve_ivp_error')
-plt.plot(t,my_erreur_sin,label='myODE45_error')
-plt.legend()
-
-#plt.show()
-
-
-print('total_time_expo = ')
-print(total_time_expo)
-print('my_total_time_expo = ')
-print(my_total_time_expo)
-
-print('total_time_sin = ')
-print(total_time_sin)
-print('my_total_time_sin = ')
-print(my_total_time_sin)
-
-######################################### Nbrs laisser choisit par le solveur
-
-######################################### Approximation par solve_ivp
-print("===================")
-
-tps1_expo = time.perf_counter()
-sol_expo = solve_ivp(diff_expo, [0, 10], [2])
-tps2_expo = time.perf_counter()
-
-tps1_sin = time.perf_counter()
-sol_sin = solve_ivp(diff_sin, [0, 10], [0])
-tps2_sin = time.perf_counter()
-
-total_time_expo = tps2_expo - tps1_expo
-total_time_sin = tps2_sin - tps1_sin
-
-############ Evaluation exacte
-true_sol_expo = np.zeros(np.size(sol_expo.t))
-true_sol_sin = np.zeros(np.size(sol_sin.t))
-
-for i in range(np.size(sol_expo.t)) :
-    true_sol_expo[i] = expo(sol_expo.t[i])
-for i in range(np.size(sol_sin.t)) :
-    true_sol_sin[i] = sin(sol_sin.t[i])
-############
-
-erreur_expo = np.abs(true_sol_expo - sol_expo.y[0])
-erreur_sin = np.abs(true_sol_sin - sol_sin.y[0])
-    
-######################################### Mon approximation
-print("===================")
-
-my_tps1_expo = time.perf_counter()
-(my_sol_expo_t,my_sol_expo_y) = myODE45(diff_expo, [0, 10], [2], None)
-my_tps2_expo = time.perf_counter()
-
-my_tps1_sin = time.perf_counter()
-(my_sol_sin_t,my_sol_sin_y) = myODE45(diff_sin, [0, 10], [0], None)
-my_tps2_sin = time.perf_counter()
-
-my_total_time_expo = my_tps2_expo - my_tps1_expo
-my_total_time_sin = my_tps2_sin - my_tps1_sin
-
-############ Evaluation exacte
-true_sol_expo = np.zeros(np.size(my_sol_expo_t))
-true_sol_sin = np.zeros(np.size(my_sol_sin_t))
-for i in range(np.size(my_sol_expo_t)) :
-    true_sol_expo[i] = expo(my_sol_expo_t[0][i])
-for i in range(np.size(my_sol_sin_t)) :
-    true_sol_sin[i] = sin(my_sol_sin_t[0][i])
-############
-
-my_erreur_expo = np.abs(true_sol_expo - my_sol_expo_y[0])
-my_erreur_sin = np.abs(true_sol_sin - my_sol_sin_y[0])
-
-fig3 = plt.figure()
-plt.title('EDO : dy/dt = -0.5 * y         y(0) = 2         sol : y(t) = 2*exp(-t/2)')
-plt.xlabel('t')
-plt.ylabel('approximation_error')
-plt.yscale('log')
-plt.plot(sol_expo.t,erreur_expo,label='solve_ivp_error')
-plt.plot(my_sol_expo_t[0],my_erreur_expo,label='myODE45_error')
-plt.legend()
-
-fig4 = plt.figure()
-plt.title('EDO : dy/dt = cos(t)       y(0) = 0        sol : y(t) = sin(t)')
-plt.xlabel('t')
-plt.ylabel('approximation_error')
-plt.yscale('log')
-plt.plot(sol_sin.t,erreur_sin,label='solve_ivp_error')
-plt.plot(my_sol_sin_t[0],my_erreur_sin,label='myODE45_error')
-plt.legend()
-
-print('total_time_expo = ')
-print(total_time_expo)
-print('my_total_time_expo = ')
-print(my_total_time_expo)
-
-print('total_time_sin = ')
-print(total_time_sin)
-print('my_total_time_sin = ')
-print(my_total_time_sin)
-
 plt.show()
