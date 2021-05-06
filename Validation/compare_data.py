a6abd05561b8652da93e574596a826b9583bf9f2
armoreau <arthur.moreau@student.uclouvain.be>
2021-05-06 14:31:55 +0200

    Add files via upload

---
+++ Validation/compare_data.py
@@ -0,0 +1,24 @@
+import numpy as np
+import math
+
+#arrondi un vecteur avec digit chiffre significatif
+def signif(x, digit):
+    toRet = np.zeros(len(x))
+    for i in range(len(x)):
+        if x[i] == 0:
+            toRet[i] = 0
+        else :
+            toRet[i] = round(x[i], digit - int(math.floor(math.log10(abs(x[i])))) - 1)
+    return toRet
+
+#On peut aussi regarder la différence si |data_Matlab-data_python| < tol. avec tol= 10e-3
+def compare_data(MatlabFileName, PythonFileName, digit):
+        
+    data_Matlab = np.array([ float(z) for z in open(MatlabFileName, 'r').read().split() ])
+    data_python = np.array([ float(z) for z in open(PythonFileName, 'r').read().split() ])
+    
+    rounded_data_Matlab = signif(data_Matlab, digit)
+    rounded_data_python = signif(data_python, digit)
+    
+    Boolean_vector = rounded_data_Matlab == rounded_data_python
+    return Boolean_vector.all()
---
+++ Validation/verification.py
@@ -0,0 +1,346 @@
+import numpy as np
+from compare_data import compare_data
+
+######################################### Verification
+
+Test1_t = Test1_y = 0
+Test2_t = Test2_y = 0
+Test3_t = Test3_y = 0
+Test4_t = Test4_y = 0
+Test5_t = Test5_y = 0
+Test6_t = Test6_y = 0
+Test7_t = Test7_y = 0
+Test8_t = Test8_y = 0
+Test9_t = Test9_y = 0
+Test10_t = Test10_y = 0
+Test11_t = Test11_y = 0
+Test12_t = Test12_y = 0
+Test13_t = Test13_y = 0
+Test14_t = Test14_y = 0
+Test15_t = Test15_y = 0
+Test16_t = Test16_y = 0
+Test17_t = Test17_y = 0
+Test18_t = Test18_y = 0
+Test19_t = Test19_y = 0
+Test20_t = Test20_y = 0
+Test21_t = Test21_y = 0
+Test22_t = Test22_y = 0
+Test23_t = Test23_y = 0
+Test24_t = Test24_y = 0
+Test25_t = Test25_y = 0
+Test26_t = Test26_y = 0
+Test27_t = Test27_y = 0
+Test28_t = Test28_y = 0
+Test29_t = Test29_y = 0
+Test30_t = Test30_y = 0
+Test31_t = Test31_y = 0
+Test32_t = Test32_y = 0
+Test33_t = Test33_y = 0
+Test34_t = Test34_y = 0
+Test35_t = Test35_y = 0
+Test36_t = Test36_y = 0
+Test37_t = Test37_y = 0
+Test38_t = Test38_y = 0
+Test39_t = Test39_y = 0
+Test40_t = Test40_y = 0
+Test41_t = Test41_y = 0
+
+for digit in [1, 2, 3 , 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] :
+    Test1_Boolean_t = compare_data('Test1_python_t.txt','Test1_Matlab_t.txt', digit)
+    Test1_Boolean_y  = compare_data('Test1_python_y.txt','Test1_Matlab_y.txt', digit)
+
+    Test2_Boolean_t = compare_data('Test2_python_t.txt','Test2_Matlab_t.txt', digit)
+    Test2_Boolean_y  = compare_data('Test2_python_y.txt','Test2_Matlab_y.txt', digit)
+
+    Test3_Boolean_t = compare_data('Test3_python_t.txt','Test3_Matlab_t.txt', digit)
+    Test3_Boolean_y  = compare_data('Test3_python_y.txt','Test3_Matlab_y.txt', digit)
+
+    Test4_Boolean_t = compare_data('Test4_python_t.txt','Test4_Matlab_t.txt', digit)
+    Test4_Boolean_y  = compare_data('Test4_python_y.txt','Test4_Matlab_y.txt', digit)
+
+    Test5_Boolean_t = compare_data('Test5_python_t.txt','Test5_Matlab_t.txt', digit)
+    Test5_Boolean_y  = compare_data('Test5_python_y.txt','Test5_Matlab_y.txt', digit)
+
+    Test6_Boolean_t = compare_data('Test6_python_t.txt','Test6_Matlab_t.txt', digit)
+    Test6_Boolean_y  = compare_data('Test6_python_y.txt','Test6_Matlab_y.txt', digit)
+
+    Test7_Boolean_t = compare_data('Test7_python_t.txt','Test7_Matlab_t.txt', digit)
+    Test7_Boolean_y  = compare_data('Test7_python_y.txt','Test7_Matlab_y.txt', digit)
+
+    Test8_Boolean_t = compare_data('Test8_python_t.txt','Test8_Matlab_t.txt', digit)
+    Test8_Boolean_y  = compare_data('Test8_python_y.txt','Test8_Matlab_y.txt', digit)
+
+    Test9_Boolean_t = compare_data('Test9_python_t.txt','Test9_Matlab_t.txt', digit)
+    Test9_Boolean_y  = compare_data('Test9_python_y.txt','Test9_Matlab_y.txt', digit)
+
+    Test10_Boolean_t = compare_data('Test10_python_t.txt','Test10_Matlab_t.txt', digit)
+    Test10_Boolean_y  = compare_data('Test10_python_y.txt','Test10_Matlab_y.txt', digit)
+
+    Test11_Boolean_t = compare_data('Test11_python_t.txt','Test11_Matlab_t.txt', digit)
+    Test11_Boolean_y  = compare_data('Test11_python_y.txt','Test11_Matlab_y.txt', digit)
+
+    Test12_Boolean_t = compare_data('Test12_python_t.txt','Test12_Matlab_t.txt', digit)
+    Test12_Boolean_y  = compare_data('Test12_python_y.txt','Test12_Matlab_y.txt', digit)
+
+    Test13_Boolean_t = compare_data('Test13_python_t.txt','Test13_Matlab_t.txt', digit)
+    Test13_Boolean_y  = compare_data('Test13_python_y.txt','Test13_Matlab_y.txt', digit)
+
+    Test14_Boolean_t = compare_data('Test14_python_t.txt','Test14_Matlab_t.txt', digit)
+    Test14_Boolean_y  = compare_data('Test14_python_y.txt','Test14_Matlab_y.txt', digit)
+
+    Test15_Boolean_t = compare_data('Test15_python_t.txt','Test15_Matlab_t.txt', digit)
+    Test15_Boolean_y  = compare_data('Test15_python_y.txt','Test15_Matlab_y.txt', digit)
+
+    Test16_Boolean_t = compare_data('Test16_python_t.txt','Test16_Matlab_t.txt', digit)
+    Test16_Boolean_y  = compare_data('Test16_python_y.txt','Test16_Matlab_y.txt', digit)
+
+    Test17_Boolean_t = compare_data('Test17_python_t.txt','Test17_Matlab_t.txt', digit)
+    Test17_Boolean_y  = compare_data('Test17_python_y.txt','Test17_Matlab_y.txt', digit)
+
+    Test18_Boolean_t = compare_data('Test18_python_t.txt','Test18_Matlab_t.txt', digit)
+    Test18_Boolean_y  = compare_data('Test18_python_y.txt','Test18_Matlab_y.txt', digit)
+
+    Test19_Boolean_t = compare_data('Test19_python_t.txt','Test19_Matlab_t.txt', digit)
+    Test19_Boolean_y  = compare_data('Test19_python_y.txt','Test19_Matlab_y.txt', digit)
+
+    Test20_Boolean_t = compare_data('Test20_python_t.txt','Test20_Matlab_t.txt', digit)
+    Test20_Boolean_y  = compare_data('Test20_python_y.txt','Test20_Matlab_y.txt', digit)
+
+    Test21_Boolean_t = compare_data('Test21_python_t.txt','Test21_Matlab_t.txt', digit)
+    Test21_Boolean_y  = compare_data('Test21_python_y.txt','Test21_Matlab_y.txt', digit)
+
+    Test22_Boolean_t = compare_data('Test22_python_t.txt','Test22_Matlab_t.txt', digit)
+    Test22_Boolean_y  = compare_data('Test22_python_y.txt','Test22_Matlab_y.txt', digit)
+
+    Test23_Boolean_t = compare_data('Test23_python_t.txt','Test23_Matlab_t.txt', digit)
+    Test23_Boolean_y  = compare_data('Test23_python_y.txt','Test23_Matlab_y.txt', digit)
+
+    Test24_Boolean_t = compare_data('Test24_python_t.txt','Test24_Matlab_t.txt', digit)
+    Test24_Boolean_y  = compare_data('Test24_python_y.txt','Test24_Matlab_y.txt', digit)
+
+    Test25_Boolean_t = compare_data('Test25_python_t.txt','Test25_Matlab_t.txt', digit)
+    Test25_Boolean_y  = compare_data('Test25_python_y.txt','Test25_Matlab_y.txt', digit)
+
+    Test26_Boolean_t = compare_data('Test26_python_t.txt','Test26_Matlab_t.txt', digit)
+    Test26_Boolean_y  = compare_data('Test26_python_y.txt','Test26_Matlab_y.txt', digit)
+
+    Test27_Boolean_t = compare_data('Test27_python_t.txt','Test27_Matlab_t.txt', digit)
+    Test27_Boolean_y  = compare_data('Test27_python_y.txt','Test27_Matlab_y.txt', digit)
+
+    Test28_Boolean_t = compare_data('Test28_python_t.txt','Test28_Matlab_t.txt', digit)
+    Test28_Boolean_y  = compare_data('Test28_python_y.txt','Test28_Matlab_y.txt', digit)
+
+    Test29_Boolean_t = compare_data('Test29_python_t.txt','Test29_Matlab_t.txt', digit)
+    Test29_Boolean_y  = compare_data('Test29_python_y.txt','Test29_Matlab_y.txt', digit)
+
+    Test30_Boolean_t = compare_data('Test30_python_t.txt','Test30_Matlab_t.txt', digit)
+    Test30_Boolean_y  = compare_data('Test30_python_y.txt','Test30_Matlab_y.txt', digit)
+
+    Test31_Boolean_t = compare_data('Test31_python_t.txt','Test31_Matlab_t.txt', digit)
+    Test31_Boolean_y  = compare_data('Test31_python_y.txt','Test31_Matlab_y.txt', digit)
+
+    Test32_Boolean_t = compare_data('Test32_python_t.txt','Test32_Matlab_t.txt', digit)
+    Test32_Boolean_y  = compare_data('Test32_python_y.txt','Test32_Matlab_y.txt', digit)
+
+    Test33_Boolean_t = compare_data('Test33_python_t.txt','Test33_Matlab_t.txt', digit)
+    Test33_Boolean_y  = compare_data('Test33_python_y.txt','Test33_Matlab_y.txt', digit)
+
+    Test34_Boolean_t = compare_data('Test34_python_t.txt','Test34_Matlab_t.txt', digit)
+    Test34_Boolean_y  = compare_data('Test34_python_y.txt','Test34_Matlab_y.txt', digit)
+
+    Test35_Boolean_t = compare_data('Test35_python_t.txt','Test35_Matlab_t.txt', digit)
+    Test35_Boolean_y  = compare_data('Test35_python_y.txt','Test35_Matlab_y.txt', digit)
+
+    Test36_Boolean_t = compare_data('Test36_python_t.txt','Test36_Matlab_t.txt', digit)
+    Test36_Boolean_y  = compare_data('Test36_python_y.txt','Test36_Matlab_y.txt', digit)
+
+    Test37_Boolean_t = compare_data('Test37_python_t.txt','Test37_Matlab_t.txt', digit)
+    Test37_Boolean_y  = compare_data('Test37_python_y.txt','Test37_Matlab_y.txt', digit)
+
+    Test38_Boolean_t = compare_data('Test38_python_t.txt','Test38_Matlab_t.txt', digit)
+    Test38_Boolean_y  = compare_data('Test38_python_y.txt','Test38_Matlab_y.txt', digit)
+
+    Test39_Boolean_t = compare_data('Test39_python_t.txt','Test39_Matlab_t.txt', digit)
+    Test39_Boolean_y  = compare_data('Test39_python_y.txt','Test39_Matlab_y.txt', digit)
+
+    Test40_Boolean_t = compare_data('Test40_python_t.txt','Test40_Matlab_t.txt', digit)
+    Test40_Boolean_y  = compare_data('Test40_python_y.txt','Test40_Matlab_y.txt', digit)
+
+    Test41_Boolean_t = compare_data('Test41_python_t.txt','Test41_Matlab_t.txt', digit)
+    Test41_Boolean_y  = compare_data('Test41_python_y.txt','Test41_Matlab_y.txt', digit)
+    
+    if (Test1_Boolean_t) :
+        Test1_t += 1
+    if (Test2_Boolean_t) :
+        Test2_t += 1
+    if (Test3_Boolean_t) :
+        Test3_t += 1
+    if (Test4_Boolean_t) :
+        Test4_t += 1
+    if (Test5_Boolean_t) :
+        Test5_t += 1
+    if (Test6_Boolean_t) :
+        Test6_t += 1
+    if (Test7_Boolean_t) :
+        Test7_t += 1
+    if (Test8_Boolean_t) :
+        Test8_t += 1
+    if (Test9_Boolean_t) :
+        Test9_t += 1
+    if (Test10_Boolean_t) :
+        Test10_t += 1
+    if (Test11_Boolean_t) :
+        Test11_t += 1
+    if (Test12_Boolean_t) :
+        Test12_t += 1
+    if (Test13_Boolean_t) :
+        Test13_t += 1
+    if (Test14_Boolean_t) :
+        Test14_t += 1
+    if (Test15_Boolean_t) :
+        Test15_t += 1
+    if (Test16_Boolean_t) :
+        Test16_t += 1
+    if (Test17_Boolean_t) :
+        Test17_t += 1
+    if (Test18_Boolean_t) :
+        Test18_t += 1
+    if (Test19_Boolean_t) :
+        Test19_t += 1
+    if (Test20_Boolean_t) :
+        Test20_t += 1
+    if (Test21_Boolean_t) :
+        Test21_t += 1
+    if (Test22_Boolean_t) :
+        Test22_t += 1
+    if (Test23_Boolean_t) :
+        Test23_t += 1
+    if (Test24_Boolean_t) :
+        Test24_t += 1
+    if (Test25_Boolean_t) :
+        Test25_t += 1
+    if (Test26_Boolean_t) :
+        Test26_t += 1
+    if (Test27_Boolean_t) :
+        Test27_t += 1
+    if (Test28_Boolean_t) :
+        Test28_t += 1
+    if (Test29_Boolean_t) :
+        Test29_t += 1
+    if (Test30_Boolean_t) :
+        Test30_t += 1
+    if (Test31_Boolean_t) :
+        Test31_t += 1
+    if (Test32_Boolean_t) :
+        Test32_t += 1
+    if (Test33_Boolean_t) :
+        Test33_t += 1
+    if (Test34_Boolean_t) :
+        Test34_t += 1
+    if (Test35_Boolean_t) :
+        Test35_t += 1
+    if (Test36_Boolean_t) :
+        Test36_t += 1
+    if (Test37_Boolean_t) :
+        Test37_t += 1
+    if (Test38_Boolean_t) :
+        Test38_t += 1
+    if (Test39_Boolean_t) :
+        Test39_t += 1
+    if (Test40_Boolean_t) :
+        Test40_t += 1
+    if (Test41_Boolean_t) :
+        Test41_t += 1
+
+    if (Test1_Boolean_y) :
+        Test1_y += 1
+    if (Test2_Boolean_y) :
+        Test2_y += 1
+    if (Test3_Boolean_y) :
+        Test3_y += 1
+    if (Test4_Boolean_y) :
+        Test4_y += 1
+    if (Test5_Boolean_y) :
+        Test5_y += 1
+    if (Test6_Boolean_y) :
+        Test6_y += 1
+    if (Test7_Boolean_y) :
+        Test7_y += 1
+    if (Test8_Boolean_y) :
+        Test8_y += 1
+    if (Test9_Boolean_y) :
+        Test9_y += 1
+    if (Test10_Boolean_y) :
+        Test10_y += 1
+    if (Test11_Boolean_y) :
+        Test11_y += 1
+    if (Test12_Boolean_y) :
+        Test12_y += 1
+    if (Test13_Boolean_y) :
+        Test13_y += 1
+    if (Test14_Boolean_y) :
+        Test14_y += 1
+    if (Test15_Boolean_y) :
+        Test15_y += 1
+    if (Test16_Boolean_y) :
+        Test16_y += 1
+    if (Test17_Boolean_y) :
+        Test17_y += 1
+    if (Test18_Boolean_y) :
+        Test18_y += 1
+    if (Test19_Boolean_y) :
+        Test19_y += 1
+    if (Test20_Boolean_y) :
+        Test20_y += 1
+    if (Test21_Boolean_y) :
+        Test21_y += 1
+    if (Test22_Boolean_y) :
+        Test22_y += 1
+    if (Test23_Boolean_y) :
+        Test23_y += 1
+    if (Test24_Boolean_y) :
+        Test24_y += 1
+    if (Test25_Boolean_y) :
+        Test25_y += 1
+    if (Test26_Boolean_y) :
+        Test26_y += 1
+    if (Test27_Boolean_y) :
+        Test27_y += 1
+    if (Test28_Boolean_y) :
+        Test28_y += 1
+    if (Test29_Boolean_y) :
+        Test29_y += 1
+    if (Test30_Boolean_y) :
+        Test30_y += 1
+    if (Test31_Boolean_y) :
+        Test31_y += 1
+    if (Test32_Boolean_y) :
+        Test32_y += 1
+    if (Test33_Boolean_y) :
+        Test33_y += 1
+    if (Test34_Boolean_y) :
+        Test34_y += 1
+    if (Test35_Boolean_y) :
+        Test35_y += 1
+    if (Test36_Boolean_y) :
+        Test36_y += 1
+    if (Test37_Boolean_y) :
+        Test37_y += 1
+    if (Test38_Boolean_y) :
+        Test38_y += 1
+    if (Test39_Boolean_y) :
+        Test39_y += 1
+    if (Test40_Boolean_y) :
+        Test40_y += 1
+    if (Test41_Boolean_y) :
+        Test41_y += 1
+        
+Test_t = np.array([Test1_t,Test2_t,Test3_t,Test4_t,Test5_t,Test6_t,Test7_t,Test8_t,Test9_t,Test10_t,
+                   Test11_t,Test12_t,Test13_t,Test14_t,Test15_t,Test16_t,Test17_t,Test18_t,Test19_t,Test20_t,
+                   Test21_t,Test22_t,Test23_t,Test24_t,Test25_t,Test26_t,Test27_t,Test28_t,Test29_t,Test30_t,
+                   Test31_t,Test32_t,Test33_t,Test34_t,Test35_t,Test36_t,Test37_t,Test38_t,Test39_t,Test40_t,Test41_t])
+
+Test_y = np.array([Test1_y,Test2_y,Test3_y,Test4_y,Test5_y,Test6_y,Test7_y,Test8_y,Test9_y,Test10_y,
+                   Test11_y,Test12_y,Test13_y,Test14_y,Test15_y,Test16_y,Test17_y,Test18_y,Test19_y,Test20_y,
+                   Test21_y,Test22_y,Test23_y,Test24_y,Test25_y,Test26_y,Test27_y,Test28_y,Test29_y,Test30_y,
+                   Test31_y,Test32_y,Test33_y,Test34_y,Test35_y,Test36_y,Test37_y,Test38_y,Test39_y,Test40_y,Test41_y])
